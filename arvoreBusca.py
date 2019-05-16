2488cdb4b99dfd2867932482840f76719e32794d
Victor Gabriel <victorgfb80@gmail.com>
2019-05-16 09:39:14 -0300

    cria arquivo buscaCega

--- arvoreBusca.py
+++ arvoreBusca.py
@@ -73,31 +73,3 @@ class arvoreBusca():
             lista.append(novoEstado)
 
         return lista
-
-arvore = arvoreBusca([[ 1,  2,  3], [ 4,  7, 6], [ 8,  5,  0]], "largura")
-
-while(1):    
-    if(arvore.borda.vazia()):
-        print("error")
-        break
-
-    noAtual = arvore.borda.removerPrimeiro()
-    print(noAtual.profundidade)
-    for i in noAtual.estado:
-        print(i)
-
-    if(arvore.testaObjetivo(noAtual.estado)):
-        print("--------------")
-        for i in noAtual.estado:
-            print("| " + str(i) + " |")
-        print("--------------")
-        break
-    else:
-        lista = arvore.funcaoSucessora(noAtual.estado)
-        listaNos = []
-        for i, estado in enumerate(lista):
-            no = No(estado, noAtual,i, noAtual.custoCaminho, noAtual.profundidade)
-            if(not(arvore.verificaJaAberto(no))):
-                listaNos.append(no)
-                arvore.inseriJaAberto(no)
-        arvore.borda.inserirTodos(listaNos)
